d7c37f5af78450e3b9c9d4f6e6016ee0fd7d7017
Andrii <Luckyhidalgo@gmail.com>
2020-05-04 15:08:06 +0300

    Changed README file. Added dist files.

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
-# [Simbotify](http://domainName.surge.sh/)
+[![Netlify Status](https://api.netlify.com/api/v1/badges/9074b098-3f4a-49cf-ad9f-30ea041ad9ed/deploy-status)](https://app.netlify.com/sites/simbotify/deploys)
+
+# [Simbotify](https://simbotify.netlify.app/listpages)
 
 ## How to start
 * `yarn && bower i && gulp` - install npm dependencies, bower packages and run dev-server
---
+++ dest/js/app.js
@@ -0,0 +1,104 @@
+"use strict";
+
+/*
+*
+* ============================
+* ============================
+*
+* Include lib:
+*
+* - webFontLoader.js;
+* - preventBehavior.js;
+* - svg4everybody.js;
+*
+* ============================
+* ============================
+* */
+
+/**
+ * @name initPreventBehavior
+ *
+ * @description
+ */
+var initPreventBehavior = function initPreventBehavior() {
+
+  var link = document.querySelectorAll("a");
+
+  link.forEach(function (val, idx) {
+
+    val.addEventListener("click", function (e) {
+      if (val.getAttribute("href") === "#") {
+        e.preventDefault();
+      }
+    });
+  });
+};
+
+/**
+ * @name initSvg4everybody
+ *
+ * @description SVG for Everybody adds external spritemaps support to otherwise SVG-capable browsers.
+ */
+var initSvg4everybody = function initSvg4everybody() {
+
+  svg4everybody();
+};
+
+/**
+ * @name initWebFontLoader
+ *
+ * @description Loading fonts regardless of the source, then adds a standard set of events you may use to control the loading experience... for more details => https://github.com/typekit/fvd
+ */
+var initWebFontLoader = function initWebFontLoader() {
+
+  /**
+    * @description
+   */
+  WebFont.load({
+    google: {
+      families: ['Roboto:100,300,400,500,700,900']
+    }
+  });
+
+  /**
+    * @description
+   */
+  // const WebFontConfig = {
+  //   custom: {
+  //     families: [
+  //       'Lato:n1,n3,n4,n5,n6,n7,n9'
+  //     ]
+  //   }
+  // };
+};
+/**
+ * @description Document DOM ready.
+ */
+(function () {
+  /*
+  * =============================================
+  * CALLBACK :: start
+  * ============================================= */
+
+  /*
+  * CALLBACK :: end
+  * ============================================= */
+
+  /**
+   * @name initNative
+   *
+   * @description Init all method
+   */
+  var initNative = function initNative() {
+    // default
+    initPreventBehavior();
+    // ==========================================
+
+    // lib
+    // ==========================================
+
+    // callback
+    // ==========================================
+  };
+  initNative();
+})();
